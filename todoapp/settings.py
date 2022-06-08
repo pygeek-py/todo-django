657ded91bcce7ad53de5e6748c9cfa78cc7f72cd
pygeek-py <usmanmalikpg2010@gmail.com>
2022-06-08 04:44:37 +0100

    tweaking setting

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -83,11 +83,7 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD': 'malik54321',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
